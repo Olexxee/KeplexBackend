f2802f750d57e5cbb3f628437bc89b1c46cbcd9e
Olexxee <olexxee14@gmail.com>
2026-05-24 10:21:24 -0700

    Updated the cookied

--- src/modules/auth/auth.cookies.js
+++ src/modules/auth/auth.cookies.js
@@ -1,40 +1,33 @@
 import { env } from "../../config/env.js";
-
 import {
   ACCESS_TOKEN_EXPIRES_IN_MS,
   REFRESH_TOKEN_EXPIRES_IN_MS,
 } from "./auth.constants.js";
 
+const cookieOptions = (env) => ({
+  httpOnly: true,
+  secure: env.nodeEnv === "production",
+  sameSite: env.nodeEnv === "production" ? "none" : "lax",
+});
+
 export const setAccessTokenCookie = (res, token) => {
   res.cookie("accessToken", token, {
-    httpOnly: true,
-    secure: env.nodeEnv === "production",
-    sameSite: "lax",
+    ...cookieOptions(env),
     maxAge: ACCESS_TOKEN_EXPIRES_IN_MS,
   });
 };
 
 export const clearAccessTokenCookie = (res) => {
-  res.clearCookie("accessToken", {
-    httpOnly: true,
-    secure: env.nodeEnv === "production",
-    sameSite: "lax",
-  });
+  res.clearCookie("accessToken", cookieOptions(env));
 };
 
 export const setRefreshTokenCookie = (res, token) => {
   res.cookie("refreshToken", token, {
-    httpOnly: true,
-    secure: env.nodeEnv === "production",
-    sameSite: "lax",
+    ...cookieOptions(env),
     maxAge: REFRESH_TOKEN_EXPIRES_IN_MS,
   });
 };
 
 export const clearRefreshTokenCookie = (res) => {
-  res.clearCookie("refreshToken", {
-    httpOnly: true,
-    secure: env.nodeEnv === "production",
-    sameSite: "lax",
-  });
+  res.clearCookie("refreshToken", cookieOptions(env));
 };
